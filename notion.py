e5724718aeba54c5ece111b9b46f43725bb101e0
ohmygod0193 <62164142+hac-ohmygod0193@users.noreply.github.com>
2024-02-11 23:18:56 +0800

    Modify summary to notion format

--- notion.py
+++ notion.py
@@ -1,11 +1,15 @@
 from notion_client import Client
+from dotenv import load_dotenv, dotenv_values
 import os
+
+load_dotenv()
 # Initialize the client
 # 取得token的方法，可以參考官方文件這邊 https://developers.notion.com/docs/getting-started
 NOTION_TOKEN = os.environ["NOTION_TOKEN"]
 notion = Client(auth=NOTION_TOKEN)
 
 database_id = os.environ["DATABASE_ID"]
+
 
 def insert_to_notion(
     contents,
@@ -19,7 +23,7 @@ def insert_to_notion(
     # 例如 https://www.notion.so/natlee/test-4f3f55661c333e5585660c4c35e10533
     # 這邊的DATABASE_ID就是`4f3f55661c333e5585660c4c35e10533`
 
-    notion.pages.create(
+    created_page = notion.pages.create(
         **{
             "parent": {"database_id": database_id},
             "properties": {
@@ -32,27 +36,46 @@ def insert_to_notion(
                 # 建立時間，我的例子是使用像這樣的資料 `2022-04-21`
                 "Received time": {"date": {"start": date}},
             },
-            "children": [  # 這邊比較麻煩，要指定內容是哪種屬性，例如paragraph
-                {
-                    "object": "block",
-                    "type": "bulleted_list_item",
-                    "bulleted_list_item": {
-                        # 內文的屬性跟內容
-
-                        "rich_text": [{"type": "text", "text": {"content": content}}]
-                    },
-                }for content in contents
-            ],
+            "children": [],
         }
     )
+    for content in contents:
+        block_type = ""
+        index = -1
+        if len(content) == 0:
+            continue
+        if '#' in content:
+            block_type = 'heading_3'
+            index = content.find('#')
+        elif '-' in content:
+            block_type = 'bulleted_list_item'
+            index = content.find('-')
+        else:
+            continue
+        notion.blocks.children.append(
+            created_page["id"],
+            children=[
+                {
+                    "object": "block",
+                    "type": block_type,
+                    block_type: {
+                        "rich_text": [{"type": "text", "text": {"content": content[index+1:]}}]
+                    }
+                }
+            ]
+        )
 
 
 if __name__ == '__main__':
     # Replace the following variables with your actual data
     subject = "Your Subject"
     course_id = "Your Course ID"
+    teacher_name = "nan"
     sender = "Your Sender"
-    date = "2022-04-21"
-    content = "Your Content hello"
-    contents = content.split()
-    insert_to_notion(contents, subject, course_id, sender, date)
+    date = "2024-02-11"
+    content = """   
+    # Hello World
+    - i am HAC
+    """
+    contents = content.split('\n')
+    insert_to_notion(contents, subject, course_id, teacher_name, sender, date)
