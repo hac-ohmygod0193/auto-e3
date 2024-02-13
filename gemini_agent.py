54560922067d9193ffeab75084ed590730293f4e
ohmygod0193 <62164142+hac-ohmygod0193@users.noreply.github.com>
2024-02-13 23:02:25 +0800

    Fix llama_index missing PromptTemplate bug

--- gemini_agent.py
+++ gemini_agent.py
@@ -1,18 +1,20 @@
-from llama_index.prompts import PromptTemplate
 from llama_index.llms import Gemini
 import google.generativeai as genai
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
 
 GOOGLE_API_KEY = os.environ["GOOGLE_API_KEY"]
 
 genai.configure()
 
 llm = Gemini(model='models/gemini-pro')
+
 prompt = """
 Email:
 ---------------------
-{email_str}
+{}
 ---------------------
 
 Summarize this academic email for me, highlighting deadlines if there exist 
@@ -20,12 +22,8 @@ and extract important information as clear and short as possible.
 Only use headers(#) and bullet points(-) in your summary with markdown format.
 Only Use Traditional Chinese.
 """
-prompt_tmpl = PromptTemplate(prompt)
-
 
 def summarize_email(email: str):
-    fmt_prompt = prompt_tmpl.format(
-        email_str=email
-    )
+    fmt_prompt = prompt.format(email)
     response = llm.complete(fmt_prompt)
     return str(response)
